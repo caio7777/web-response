6eb4601960398714451c5a04762729f369672b02
caio bruno <caiobruno03@icloud.com>
2023-03-20 20:17:03 -0300

    final-3

--- index.js
+++ index.js
@@ -9,23 +9,21 @@ const img1 = document
 const sound2 = new Audio("./85599__jankoehl__splash01.wav");
 
 const img2 = document
-  .querySelector('.icon')
-  .addEventListener('click', () => {
+  .querySelector('#icon-1')
+  .addEventListener('mouseover', () => {
     sound2.play();
   });
 
   const sound3 = new Audio("./85599__jankoehl__splash01.wav");
 
 const img3 = document
-  .querySelector('.icon')
+  .querySelector('#icon-2')
   .addEventListener("mouseover", () => {
     sound3.play();
   });
 
   const sound4 = new Audio("./85599__jankoehl__splash01.wav");
 
-const img4 = document
-  .querySelector('.icon')
-  .addEventListener("mouseover", () => {
+const img4 = document.querySelector('#icon-3').addEventListener("mouseover", () => {
     sound4.play();
-  });
+});
