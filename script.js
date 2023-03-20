e93372a7a334d96ef92f57d74498eda639d1bbe8
caio bruno <caiobruno03@icloud.com>
2023-03-20 18:39:56 -0300

    finish

---
+++ script.js
@@ -0,0 +1,31 @@
+const sound1 = new Audio("./429736__mateopkp8__28-desliza-papel.wav");
+
+const img1 = document
+  .querySelector(".img-perfil")
+  .addEventListener("mouseover", () => {
+    sound1.play();
+  });
+
+const sound2 = new Audio("./85599__jankoehl__splash01.wav");
+
+const img2 = document
+  .querySelector(".icon")
+  .addEventListener("mouseover", () => {
+    sound2.play();
+  });
+
+  const sound3 = new Audio("./85599__jankoehl__splash01.wav");
+
+const img3 = document
+  .querySelector(".icon1")
+  .addEventListener("mouseover", () => {
+    sound2.play();
+  });
+
+  const sound4 = new Audio("./85599__jankoehl__splash01.wav");
+
+const img4 = document
+  .querySelector(".icon2")
+  .addEventListener("mouseover", () => {
+    sound2.play();
+  });
